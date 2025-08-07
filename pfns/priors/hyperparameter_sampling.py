c80bc268b0056601f547af9681c92df8e81ecd5e
Samuel Gabriel Müller <sammuller@meta.com>
2025-08-07 14:50:02 +0200

    hyperparameter sampling fix

--- pfns/priors/hyperparameter_sampling.py
+++ pfns/priors/hyperparameter_sampling.py
@@ -225,6 +225,7 @@ def get_batch(
             batch_size // num_models,
             *args,
             hyperparameters=hyperparameters_sample,
+            batch_size_per_gp_sample=batch_size_per_gp_sample,
             **kwargs,
         )
         sub_batches.append(sub_batch)
